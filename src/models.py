6341261e5ecce9f88b7bc5acfb01f7e63648458c
XxDrancerxX <idmdiazmora98@gmail.com>
2025-04-05 01:00:16 +0000

    a

--- src/models.py
+++ src/models.py
@@ -5,15 +5,32 @@ from sqlalchemy.orm import Mapped, mapped_column
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    password = db.Column(db.String(50), unique = True, nullable = False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "user_id": self.user_id,
             # do not serialize the password, its a security breach
         }
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Integer, unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    status = db.Column(db.String(120), unique=False, nullable=False, default="active")
+
+class Friends(db.Model):
+    id = db.Column(db.Integer, primary_key = True)    
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    friend_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    status = db.Column(db.String(100), nullable=False, default="pending")  
+
+class Likes(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)       
